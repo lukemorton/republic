4a9558f5a36e9429a42c5afc4e6f9746dccd5c2f
Luke Morton <lukemorton.dev@gmail.com>
2017-08-09 22:43:05 +0100

    Implement nextHandler to make it easy to connect express and next

--- src/express.js
+++ src/express.js
@@ -11,3 +11,20 @@ export function asExpressMiddleware (app, handler) {
 
   return router
 }
+
+export function nextHandler (nextApp) {
+  return function ({ req, res, route }) {
+    const pagePath = `/${route.action.replace('#', '/')}`
+    const query = {
+      ...req.query,
+      ...req.params,
+      action: route.action
+    }
+
+    if (route.isPage()) {
+      return nextApp.render(req, res, pagePath, query)
+    } else {
+      return route.handler({ req, res, query })
+    }
+  }
+}
--- test/unit/express.test.js
+++ test/unit/express.test.js
@@ -1,6 +1,6 @@
 import supertest from 'supertest'
 import express from 'express'
-import { asExpressMiddleware } from '../../src/express'
+import { asExpressMiddleware, nextHandler } from '../../src/express'
 import Upcoming, { route } from '../../src/upcoming'
 const { GET } = route
 
@@ -23,5 +23,48 @@ describe('asExpressMiddleware', () => {
   test('it passes route to handler', async () => {
     const res = await supertest(server).get('/api/blog/posts')
     expect(handler).toBeCalledWith(expect.objectContaining({ route }))
+  })
+})
+
+describe('nextHandler', () => {
+  const req = { params: { exampleParam: 'cool' }, query: { exampleQuery: true } }
+  const res = {}
+  let next
+  let route
+
+  beforeEach(() => {
+    next = { render: jest.fn() }
+  })
+
+  function createPageRoute () {
+    return { action: 'blog#index', handler () {}, isPage: () => true }
+  }
+
+  function createActionRoute () {
+    return { action: 'blog#create', handler: jest.fn(), isPage: () => false }
+  }
+
+  test('it renders pages with next', () => {
+    const adapter = nextHandler(next)
+    adapter({ req, res, route: createPageRoute() })
+
+    expect(next.render).toBeCalledWith(
+      req,
+      res,
+      '/blog/index',
+      { exampleQuery: true, exampleParam: 'cool', action: 'blog#index' }
+    )
+  })
+
+  test('it handles actions with route handler', () => {
+    const adapter = nextHandler(next)
+    const route = createActionRoute()
+    adapter({ req, res, route })
+
+    expect(route.handler).toBeCalledWith({
+      req,
+      res,
+      query: { exampleQuery: true, exampleParam: 'cool', action: 'blog#create' }
+    })
   })
 })
