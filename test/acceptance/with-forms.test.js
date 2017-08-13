bc14e35dc84ba22962aed7777c1a4b3b628759c4
Luke Morton <lukemorton.dev@gmail.com>
2017-08-13 22:21:07 +0100

    Add acceptance test for applications with forms

---
+++ test/acceptance/with-forms.test.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import Upcoming from '../../src/upcoming'
+import { Form, Input } from '../../src/react/'
+
+describe('Application with forms', () => {
+  describe('when form has input', () => {
+    const handler = jest.fn()
+
+    const app = new Upcoming({
+      path: '/blog',
+      action: 'blog#index',
+      handler
+    })
+
+    const form = mount(
+      <Form app={app} action='blog#index'>
+        <Input name="email" />
+      </Form>
+    )
+
+    test('the form is aware of changes to input', () => {
+      form.find(Input).simulate('change', { target: { value: 'new@email.com' } })
+      expect(form.instance().values()).toEqual({ email: 'new@email.com' })
+    })
+
+    test('the form passes values to action', () => {
+      form.find(Input).simulate('change', { target: { value: 'new@email.com' } })
+      form.simulate('submit', { preventDefault () {} })
+      expect(handler).toBeCalledWith({ email: 'new@email.com' })
+    })
+  })
+})
