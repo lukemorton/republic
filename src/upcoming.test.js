85818d5cd9d31d5b0c235d5ac5c324fc245604e7
Luke Morton <lukemorton.dev@gmail.com>
2017-08-15 20:02:57 +0100

    Tidy up

--- src/upcoming.test.js
+++ src/upcoming.test.js
@@ -5,28 +5,22 @@ describe('Upcoming', () => {
     expect(route).toBeTruthy()
   })
 
-  describe('when initialising new application', () => {
-    test('it sets routes', () => {
-      const routes = ['cool', 'bob']
-      const app = new Upcoming(...routes)
-      expect(app.routes()).toContain(routes[0])
-      expect(app.routes()).toContain(routes[1])
-    })
+  test('it provides routes', () => {
+    const routes = ['cool', 'bob']
+    const app = new Upcoming(...routes)
+    expect(app.routes()).toContain(routes[0])
+    expect(app.routes()).toContain(routes[1])
   })
 
-  describe('when building actions', () => {
-    test('it registers actions', () => {
-      const route = { action: 'blog#index', handler: jest.fn() }
-      const app = new Upcoming(route)
-      expect(app.route('blog#index')).toBe(route)
-    })
+  test('it provides single route for action', () => {
+    const route = { action: 'blog#index', handler () {} }
+    const app = new Upcoming(route)
+    expect(app.route('blog#index')).toBe(route)
   })
 
-  describe('when retrieving a route handler', () => {
-    test('it returns handler for action', () => {
-      const route = { action: 'blog#index', handler: jest.fn() }
-      const app = new Upcoming(route)
-      expect(app.routeHandler('blog#index')).toBe(route.handler)
-    })
+  test('it returns handler for action', () => {
+    const route = { action: 'blog#index', handler () {} }
+    const app = new Upcoming(route)
+    expect(app.routeHandler('blog#index')).toBe(route.handler)
   })
 })
