226ea447a13439ea2aa35f657fac9944bca539b7
Luke Morton <lukemorton.dev@gmail.com>
2017-08-18 18:13:24 +0100

    Whitespace

--- src/express/nextHandler.js
+++ src/express/nextHandler.js
@@ -1,10 +1,7 @@
 export default function nextHandler (nextApp) {
   return function ({ req, res, route, params }) {
     const pagePath = `/${route.action.replace('#', '/')}`
-    const query = {
-      params,
-      action: route.action
-    }
+    const query = { params, action: route.action }
 
     if (route.isPage()) {
       return nextApp.render(req, res, pagePath, query)
