df1ddcf0a11a43b1e9272a730539f0f38f91cce7
Luke Morton <lukemorton.dev@gmail.com>
2017-09-30 16:59:55 +0100

    Add helper functions for testing next

---
+++ src/next/test.js
@@ -0,0 +1,10 @@
+import { mount } from 'enzyme'
+
+export async function pageProps (Page, action, params = {}) {
+  return Page.getInitialProps({ query: { action, params } })
+}
+
+export async function mountPage (Page, action, params = {}) {
+  const props = await pageProps(Page, action, params)
+  return mount(<Page {...props} />)
+}
