0cef698e095afa5d853045258d40a9a485f4e6e2
Luke Morton <lukemorton.dev@gmail.com>
2017-09-30 17:22:19 +0100

    Spread params

--- src/next/test.js
+++ src/next/test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { mount } from 'enzyme'
 
 export async function pageProps (Page, action, params = {}) {
-  return Page.getInitialProps({ query: { action, params } })
+  return Page.getInitialProps({ query: { action, ...params } })
 }
 
 export async function mountPage (Page, action, params = {}) {
